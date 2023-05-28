8a559bac958f0713b484d017d849a591d183b2cf
ElJuniorMC <125386345+ElJuniorMC@users.noreply.github.com>
2023-05-28 08:51:28 +0200

    Add files via upload

---
+++ clases.js
@@ -0,0 +1,121 @@
+function mostrardk() {
+    var fragmento = document.getElementById("dk");
+    fragmento.style.display = "block";
+    var elementos = document.getElementsByTagName("div");
+    for (var i = 0; i < elementos.length; i++) {
+    var elemento = elementos[i];
+    if (elemento.id !== "dk") {
+      elemento.style.display = "none";
+    }
+  }
+}
+function mostrarbrujo() {
+    var fragmento = document.getElementById("brujo");
+    fragmento.style.display = "block";
+    var elementos = document.getElementsByTagName("div");
+  for (var i = 0; i < elementos.length; i++) {
+    var elemento = elementos[i];
+    if (elemento.id !== "brujo") {
+      elemento.style.display = "none";
+    }
+  }
+}
+function mostrarchaman() {
+    var fragmento = document.getElementById("chaman");
+    fragmento.style.display = "block";
+    var elementos = document.getElementsByTagName("div");
+  for (var i = 0; i < elementos.length; i++) {
+    var elemento = elementos[i];
+    if (elemento.id !== "chaman") {
+      elemento.style.display = "none";
+    }
+  }
+}
+function mostrardruida() {
+    var fragmento = document.getElementById("druida");
+    fragmento.style.display = "block";
+    var elementos = document.getElementsByTagName("div");
+  for (var i = 0; i < elementos.length; i++) {
+    var elemento = elementos[i];
+    if (elemento.id !== "druida") {
+      elemento.style.display = "none";
+    }
+  }
+}
+function mostrarwar() {
+    var fragmento = document.getElementById("guerrero");
+    fragmento.style.display = "block";
+    var elementos = document.getElementsByTagName("div");
+  for (var i = 0; i < elementos.length; i++) {
+    var elemento = elementos[i];
+    if (elemento.id !== "guerrero") {
+      elemento.style.display = "none";
+    }
+  }
+}
+function mostrarhunter() {
+    var fragmento = document.getElementById("hunter");
+    fragmento.style.display = "block";
+    var elementos = document.getElementsByTagName("div");
+  for (var i = 0; i < elementos.length; i++) {
+    var elemento = elementos[i];
+    if (elemento.id !== "hunter") {
+      elemento.style.display = "none";
+    }
+  }
+}
+function mostrarmago() {
+    var fragmento = document.getElementById("mago");
+    fragmento.style.display = "block";
+    var elementos = document.getElementsByTagName("div");
+  for (var i = 0; i < elementos.length; i++) {
+    var elemento = elementos[i];
+    if (elemento.id !== "mago") {
+      elemento.style.display = "none";
+    }
+  }
+}
+function mostrarmonje() {
+    var fragmento = document.getElementById("monje");
+    fragmento.style.display = "block";
+    var elementos = document.getElementsByTagName("div");
+  for (var i = 0; i < elementos.length; i++) {
+    var elemento = elementos[i];
+    if (elemento.id !== "monje") {
+      elemento.style.display = "none";
+    }
+  }
+}
+function mostrarpala() {
+    var fragmento = document.getElementById("paladin");
+    fragmento.style.display = "block";
+    var elementos = document.getElementsByTagName("div");
+  for (var i = 0; i < elementos.length; i++) {
+    var elemento = elementos[i];
+    if (elemento.id !== "paladin") {
+      elemento.style.display = "none";
+    }
+  }
+}
+function mostrarpic() {
+    var fragmento = document.getElementById("picaro");
+    fragmento.style.display = "block";
+    var elementos = document.getElementsByTagName("div");
+  for (var i = 0; i < elementos.length; i++) {
+    var elemento = elementos[i];
+    if (elemento.id !== "picaro") {
+      elemento.style.display = "none";
+    }
+  }
+}
+function mostrarsacer() {
+    var fragmento = document.getElementById("sacer");
+    fragmento.style.display = "block";
+    var elementos = document.getElementsByTagName("div");
+  for (var i = 0; i < elementos.length; i++) {
+    var elemento = elementos[i];
+    if (elemento.id !== "sacer") {
+      elemento.style.display = "none";
+    }
+  }
+}
